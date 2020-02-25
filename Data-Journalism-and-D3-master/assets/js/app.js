00904535b63bc73b596b4bcf3a2d52f4f93de0c3
jbagri925 <55101038+jbagri925@users.noreply.github.com>
2020-02-24 19:46:42 -0800

    final changes

--- Data-Journalism-and-D3-master/assets/js/app.js
+++ Data-Journalism-and-D3-master/assets/js/app.js
@@ -1,149 +1,118 @@
 var svgWidth = 960;
 var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
-};
-
+var margin = {top:20, right:40, bottom: 60, left: 100};
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+// Create a SVG wrapper
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width",svgWidth)
+    .attr("height",svgHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var chart = svg.append("g");
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+d3.select(".chart").append("div").attr("class","tooltip").style("opacity",0);
 
-d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+d3.csv('healthData.csv',function(err, healthData) {
 
-// Import Data
-d3.csv("data.csv", function(err, healthData) {
-  if (err) throw err;
-console.log(healthData)
-  // Step 1: Parse Data/Cast as numbers
-   // ==============================
-  healthData.forEach(function(data) {
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;
-  });
+    if (err) throw err;
 
-  // Step 2: Create scale functions
-  // ==============================
-  var xLinearScale = d3.scaleLinear().range([0, width]);
-  var yLinearScale = d3.scaleLinear().range([height, 0]);
-
-  // Step 3: Create axis functions
-  // ==============================
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
-
-  var xMin;
-  var xMax;
-  var yMin;
-  var yMax;
-  
-  xMin = d3.min(healthData, function(data) {
-      return data.healthcare;
-  });
-  
-  xMax = d3.max(healthData, function(data) {
-      return data.healthcare;
-  });
-  
-  yMin = d3.min(healthData, function(data) {
-      return data.poverty;
-  });
-  
-  yMax = d3.max(healthData, function(data) {
-      return data.poverty;
-  });
-  
-  xLinearScale.domain([xMin, xMax]);
-  yLinearScale.domain([yMin, yMax]);
-  console.log(xMin);
-  console.log(yMax);
-
-  // Step 4: Append Axes to the chart
-  // ==============================
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
-
-  chartGroup.append("g")
-    .call(leftAxis);
-
-   // Step 5: Create Circles
-  // ==============================
-  var circlesGroup = chartGroup.selectAll("circle")
-  .data(healthData)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.healthcare +1.5))
-  .attr("cy", d => yLinearScale(d.poverty +0.3))
-  .attr("r", "12")
-  .attr("fill", "blue")
-  .attr("opacity", .5)
-
-  .on("mouseout", function(data, index) {
-    toolTip.hide(data);
-  });
-  // Step 6: Initialize tool tip
-  // ==============================
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (abbr + '%');
-      });
-
-  // Step 7: Create tooltip in the chart
-  // ==============================
-  chartGroup.call(toolTip);
-
-  // Step 8: Create event listeners to display and hide the tooltip
-  // ==============================
-  circlesGroup.on("click", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
+    healthData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.smokes = +data.smokes;
     });
 
-  // Create axes labels
+    
+    var yLinearScale = d3.scaleLinear().range([height,0]);
+    var xLinearScale = d3.scaleLinear().range([0,width]);
 
-  chartGroup.append("text")
-  .style("font-size", "12px")
-  .selectAll("tspan")
-  .data(healthData)
-  .enter()
-  .append("tspan")
-      .attr("x", function(data) {
-          return xLinearScale(data.healthcare +1.3);
-      })
-      .attr("y", function(data) {
-          return yLinearScale(data.poverty +.1);
-      })
-      .text(function(data) {
-          return data.abbr
-      });
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .text("Lacks Healtcare(%)");
+    //scale
 
-  chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    .attr("class", "axisText")
-    .text("In Poverty (%)");
+    xLinearScale.domain([7,d3.max(healthData, function(data) {
+            return +data.poverty;
+        }),
+    ]);
+    yLinearScale.domain([0, d3.max(healthData, function(data) {
+        return +data.smokes;
+        }),
+    ]);
+
+
+    var toolTip = d3
+        .tip()
+        .attr("class", "toolTip")
+        .offset([80,-60])
+        .html(function(data){
+            var stateName = data.state;
+            var pov = +data.poverty;
+            var smoke = +data.smokes;
+            return(stateName + "<br> Poverty(%): " + pov + "<br> Smoker Age (Median) " + smoke
+            );
+        });
+    chart.call(toolTip);
+
+    chart
+        .selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx",function(data, index){
+            return xLinearScale(data.poverty);
+        })
+        .attr("cy",function(data, index){
+            return yLinearScale(data.smokes);
+        })
+        .attr("r", "20")
+        .attr("stroke", "black")
+        .attr("opacity", 0.75)
+        .attr("fill", "salmon")
+        .on("mouseover", function(data ) {
+            toolTip.show(data,this);
+        })
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data,this);
+        });
+    
+    chart
+        .append("g")
+        .attr('transform', `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chart.append("g").call(leftAxis);
+
+    svg.selectAll(".dot")
+    .data(healthData)
+    .enter()
+    .append("text")
+    .text(function(data){return data.abbr;})
+    .attr("x", function(data){
+        return xLinearScale(data.poverty);
+    })
+    .attr("y", function(data) {
+        return yLinearScale(data.smokes);
+    })
+    .attr("font-size","10px")
+    .attr("fill","blue")
+    .style("text-anchor","middle");
+    
+    chart
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - height / 2)
+        .attr("dy","1em")
+        .attr("class", "axisText")
+        .text("Median Age of Smokers");
+
+    chart
+        .append("text")
+        .attr("transform","translate(" + width / 2 + " , " + (height + margin.top + 30) + ")",)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
 });
